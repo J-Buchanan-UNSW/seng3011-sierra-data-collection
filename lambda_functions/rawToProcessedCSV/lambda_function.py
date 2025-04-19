47b0fe6a0bdfd2bba72ef16ba210078c1f2ae963
Jack Buchanan <z5478552@ad.unsw.edu.au>
2025-04-19 21:56:34 +1000

    Added whitespace after a :

--- lambda_functions/rawToProcessedCSV/lambda_function.py
+++ lambda_functions/rawToProcessedCSV/lambda_function.py
@@ -115,7 +115,7 @@ def lambda_handler(event, _context):
             "statusCode": 200,
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps({
-                "status":"CSV processed and amended successfully"
+                "status": "CSV processed and amended successfully"
             }),
         }
 
