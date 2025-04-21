87cc266a4fa39787b62c138402fc63f995ecee3e
Bochen <z5591501@ad.unsw.edu.au>
2025-04-22 07:52:10 +1000

    added scraping2.py creates new csv file containing the esg scores scrapped from yahoo finance

--- web_scraper/scraping2.py
+++ web_scraper/scraping2.py
@@ -14,9 +14,6 @@ from bs4 import BeautifulSoup
 import re
 
 
-
-
-
 def scrape_stock(driver, ticker_symbol):
     # build the URL of the target page
     url = f'https://finance.yahoo.com/quote/{ticker_symbol}/sustainability/'
@@ -25,60 +22,74 @@ def scrape_stock(driver, ticker_symbol):
     driver.get(url)
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
+    findName = "None"
+    findTscore = "None"
+    findEscore = "None"
+    findSscore = "None"
+    findGscore = "None"
+    # entirepageFinders
+    ESGContainer = soup.find('main', {"id": "nimbus-app"})
+    CompanyDetails = soup.find('section', {"data-testid": "quote-hdr"})
 
-    with open('RawNews.csv', 'w', newline= '', encoding = 'utf8') as f:
-# set up writer for CSV file
-        writeFile = writer(f)
-        header = ['TotalScore','EnvironmentalScore','SocialScore','GovernanceScore']
-        writeFile.writerow(header)
-        print('none')
-        # with open("page_source.html", "w", encoding="utf-8") as file:
-        #     file.write(driver.page_source)
-        ESGContainer = soup.find('main', {"id": "nimbus-app"})
-        MainScoring = ESGContainer.find('section', {"data-testid": "esg-cards"})
+    # Controversy = soup.find('section', {"data-testid": "esg-controversy"})
+    # print(Controversy.get_text(strip = True))
+    # subFinders
+    CompanyName = CompanyDetails.find('h1')
+    findName = CompanyName.get_text(strip = True)
+    print(findName)
+    MainScoring = ESGContainer.find('section', {"data-testid": "esg-cards"})
+    if MainScoring:
+        # get total esg score
         TotalEsgScore = MainScoring.find('section',{"data-testid": "TOTAL_ESG_SCORE"})
 
         if TotalEsgScore:
             findTscore = TotalEsgScore.find('h4').get_text(strip = True)
             print(findTscore)
         else:
-            findTscore = None
             print("none")
+        # get Environmental esg score
         EnvironmentalScore = MainScoring.find('section',{"data-testid": "ENVIRONMENTAL_SCORE"})
         if EnvironmentalScore:
             findEscore = EnvironmentalScore.find('h4').get_text(strip = True)
             print(findEscore)
         else:
-            findEscore = None
             print("none")
+        # get Social esg score
         SocialScore = MainScoring.find('section',{"data-testid": "SOCIAL_SCORE"})
         if SocialScore:
             findSscore = SocialScore.find('h4').get_text(strip = True)
             print(findSscore)
         else:
-            findSscore = None
             print("none")
+        # get Governance esg score
         GovernanceScore = MainScoring.find('section',{"data-testid": "GOVERNANCE_SCORE"})
         if GovernanceScore:
             findGscore = GovernanceScore.find('h4').get_text(strip = True)
             print(findGscore)
         else:
-            findGscore = None
             print("none")
-        createRows = [findTscore, findEscore, findSscore, findGscore]
-        writeFile.writerow(createRows)
+    else:
+        print("No Sustainability Available")
+    createRows = [findName, findTscore, findEscore, findSscore, findGscore]
+    writeFile.writerow(createRows)
 
-# if there are no CLI parameters
-if len(sys.argv) <= 1:
-    print('Ticker symbol CLI argument missing!')
-    sys.exit(2)
+chrome_prefs = {
+    "profile.default_content_setting_values": {
+        "images": 2,
+        "stylesheet": 2,
+        "fonts": 2,
+        "plugins": 2,
+        "notifications": 2
+    }
+}
 
 options = Options()
+options.add_experimental_option("prefs", chrome_prefs)
 options.add_argument('--headless=new')
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--window-size=800x600")
+options.add_argument("--window-size=100x100")
 
 # initialize a web driver instance to control a Chrome window
 driver = webdriver.Chrome(
@@ -86,8 +97,20 @@ driver = webdriver.Chrome(
     options=options
 )
 
-# scraping all market securities
-for ticker_symbol in sys.argv[1:]:
-    scrape_stock(driver, ticker_symbol)
+# when conveted to lambda needs change
+
+with open('Tickers.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    with open('RawNews.csv', 'w', newline= '', encoding = 'utf8') as f:
+# set up writer for CSV file
+        writeFile = writer(f)
+        header = ['CompanyName','TotalScore','EnvironmentalScore','SocialScore','GovernanceScore']
+        writeFile.writerow(header)
+        for row in reader:
+            if row:
+                ticker = row[0].strip()
+                scrape_stock(driver, ticker)
+
+
 # close the browser and free up the resources
 driver.quit()
